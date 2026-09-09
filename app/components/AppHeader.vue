5f494a1f54f7e91e5c1ab5424cc684b009239c72
twentytwo777 <torukas07@gmail.com>
2026-09-09 20:33:47 +0400

    feat(ui): complete AppHeader component

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -12,6 +12,7 @@ header {
   align-items: center;
   justify-content: space-between;
 
+  column-gap: 30px;
   padding: 18px 0;
 
   background-color: var(--background);
@@ -20,6 +21,9 @@ header {
 
 header .header-title {
   display: flex;
+  flex-shrink: 0;
+  align-items: center;
+
   column-gap: 30px;
 }
 
@@ -28,7 +32,9 @@ header .header-title {
 }
 
 .header-title .title-reload {
-  width: 40px;
+  min-width: 40px;
+  max-width: 40px;
+  max-height: 40px;
   aspect-ratio: 1;
 
   display: flex;
@@ -40,6 +46,76 @@ header .header-title {
   border-radius: 50%;
   background-color: #FFFFFF;
 }
+
+.title-reload svg {
+  transition: 0.3s transform;
+}
+
+.title-reload:hover svg {
+  transform: rotate(45deg);
+}
+
+.title-reload.loading svg {
+  animation: loading 1s linear infinite;
+}
+
+@keyframes loading {
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+header .header-search {
+  width: 320px;
+  min-width: 0;
+
+  height: 40px;
+
+  display: flex;
+  flex-shrink: 1;
+  justify-content: space-between;
+
+  padding: 10px;
+  column-gap: 10px;
+
+  box-shadow: 0px 1px 4px 0px rgba(0, 0, 0, 0.05),
+  0px 2px 4px 0px rgba(0, 0, 0, 0.05);
+  border-radius: var(--radius);
+  background-color: var(--card);
+}
+
+.header-search .search-input {
+  min-width: inherit;
+
+  font-size: 16px;
+
+  flex: 1;
+}
+
+@media (max-width: 576px) {
+  header {
+    align-items: initial;
+    flex-direction: column;
+
+    row-gap: 20px;
+  }
+
+  header .header-title {
+    justify-content: space-between;
+  }
+
+  .header-title .title-value {
+    font-size: 24px;
+  }
+
+  header .header-search {
+    width: 100%;
+  }
+}
 </style>
 
 <template>
@@ -50,9 +126,9 @@ header .header-title {
         <ReloadIcon />
       </button>
     </div>
-    <div class="header-search">
-      <input type="text">
+    <label for="search" class="header-search">
+      <input name="search" id="search" type="text" class="search-input">
       <SearchIcon />
-    </div>
+    </label>
   </header>
 </template>
