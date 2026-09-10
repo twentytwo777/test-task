480627246b8825f38c0dc946a267353b74e5c589
twentytwo777 <torukas07@gmail.com>
2026-09-10 12:09:32 +0400

    feat(AppHeader): add submenu and switch to grid layout

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,30 +1,36 @@
 <script setup>
 import ReloadIcon from '~/assets/img/reload.svg';
 import SearchIcon from '~/assets/img/search.svg';
+import RowIcon from '~/assets/img/list/row.svg';
+import BlockIcon from '~/assets/img/list/block.svg';
 </script>
 
 <style scoped>
 header {
+  --column-gap: clamp(10px, 2.14dvw, 30px);
+
   position: sticky;
   top: 0;
 
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+  display: grid;
+  grid-template-rows: repeat(2, 1fr);
+  grid-template-columns: 1fr minmax(0, 320px);
 
-  column-gap: 30px;
-  padding: 18px 0;
+  align-items: center;
+
+  gap: 15px var(--column-gap);
+  padding: 15px 0;
 
   background-color: var(--background);
-  border-bottom: 1px solid #E5E5E5;
 }
 
 header .header-title {
+  min-width: max-content;
+
   display: flex;
-  flex-shrink: 0;
   align-items: center;
 
-  column-gap: 30px;
+  column-gap: var(--column-gap);
 }
 
 .header-title .title-value {
@@ -32,9 +38,7 @@ header .header-title {
 }
 
 .header-title .title-reload {
-  min-width: 40px;
-  max-width: 40px;
-  max-height: 40px;
+  width: 40px;
   aspect-ratio: 1;
 
   display: flex;
@@ -70,20 +74,17 @@ header .header-title {
 }
 
 header .header-search {
-  width: 320px;
   min-width: 0;
-
   height: 40px;
 
   display: flex;
-  flex-shrink: 1;
   justify-content: space-between;
 
   padding: 10px;
   column-gap: 10px;
 
   box-shadow: 0px 1px 4px 0px rgba(0, 0, 0, 0.05),
-  0px 2px 4px 0px rgba(0, 0, 0, 0.05);
+    0px 2px 4px 0px rgba(0, 0, 0, 0.05);
   border-radius: var(--radius);
   background-color: var(--card);
 }
@@ -96,10 +97,52 @@ header .header-search {
   flex: 1;
 }
 
+header .header-submenu {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  grid-column: span 2;
+
+  padding-top: 15px;
+  border-top: 1px solid #E5E5E5;
+}
+
+.header-submenu .submenu-portals {
+  display: flex;
+  column-gap: 15px;
+}
+
+.submenu-portals .portals-item {
+  font-weight: 700;
+  transition: 0.3s color;
+}
+
+.submenu-portals .portals-item:hover,
+.submenu-portals .portals-item.active {
+  color: var(--accent-foreground);
+}
+
+.header-submenu .submenu-view {
+  display: flex;
+  column-gap: 10px;
+}
+
+.view-button svg {
+  color: #C4C4C4;
+  transition: 0.3s color;
+}
+
+.view-button:hover svg,
+.view-button.active svg {
+  color: var(--accent-foreground);
+}
+
 @media (max-width: 576px) {
   header {
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(3, 1fr);
     align-items: initial;
-    flex-direction: column;
 
     row-gap: 20px;
   }
@@ -114,6 +157,10 @@ header .header-search {
 
   header .header-search {
     width: 100%;
+  }
+
+  header .header-submenu {
+    grid-area: initial;
   }
 }
 </style>
@@ -130,5 +177,20 @@ header .header-search {
       <input name="search" id="search" type="text" class="search-input">
       <SearchIcon />
     </label>
+    <div class="header-submenu">
+      <div class="submenu-portals">
+        <button class="portals-item">Все</button>
+        <button class="portals-item">Lenta.ru</button>
+        <button class="portals-item">Mos.ru</button>
+      </div>
+      <div class="submenu-view">
+        <button class="view-button">
+          <RowIcon />
+        </button>
+        <button class="view-button">
+          <BlockIcon />
+        </button>
+      </div>
+    </div>
   </header>
 </template>
