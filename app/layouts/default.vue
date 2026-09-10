597dc9f570a8907f937cb7b571c4843b94859b06
twentytwo777 <torukas07@gmail.com>
2026-09-10 22:22:55 +0400

    refactor(layouts): migrate default layout to grid

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,23 +1,42 @@
 <style scoped>
 .default {
+  --layout-padding: 20px;
+  --content-size: 1060px;
+  /* 40px is padding for left/right side (20px for each) */
+  --content-dynamic-size: calc(100dvw - (var(--layout-padding) * 2));
+
   width: 100dvw;
   min-height: 100dvh;
 
-  display: flex;
-  align-items: center;
-  flex-direction: column;
+  display: grid;
+  /*
+    value: 170px
+    calcs:
+    - subtract 8px, because on the layout we have 520x256, but true ratio 2 / 1 is 520x260,
+      256x2 = 512, 260x2 = 520, 520 - 512 = 8px
+    - subtract 8px, because added additional padding-(top/bottom) for card box-shadow in
+      content block
+    result: 154px
+  */
+  grid-template-rows: 154px 1fr 40px;
+  justify-content: center;
 }
 
 .default > * {
-  /* 40px is padding for left/right side (20px for each) */
-  width: min(calc(100dvw - 40px), 1060px);
+  width: min(var(--content-dynamic-size), var(--content-size));
 }
 
 .default main {
-  flex: 1;
+  height: 100%;
 
   display: flex;
   flex-direction: column;
+}
+
+@media (max-width: 576px) {
+  .default {
+    grid-template-rows: auto 1fr 40px;
+  }
 }
 </style>
 
